55100e25e49c1337a631775002048af6e3b5f5b3
mohamed.daif <daif.control@gmail.com>
2025-01-19 21:06:46 +0100

    Create a proof of concept with the Greens
    Given the static question and a single PDF, the program
    should be able to determine the top 5 priorities of the
    Green party independently.

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_community.vectorstores import FAISS
+
+
 def main():
-    print("Hello from wahl-kumpel!")
+    print("Loading the Greens electoral program")
+    pdf_path = "./documents/gruene.pdf"
+    loader = PyPDFLoader(file_path=pdf_path)
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=100, separators=["\n\n","\n"," ",""],
+    )
+    docs = text_splitter.split_documents(documents=documents)
+
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_documents(docs, embeddings)
+
+    question = """
+    I want you to write down the top 5 priorities of the Greens party if elected in 2025.
+    You should write with each point the concrete steps on how they will implement that point.
+    Write your answer in English.
+    """
+
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    combine_docs_chain = create_stuff_documents_chain(
+        ChatOpenAI(), retrieval_qa_chat_prompt
+    )
+    retrieval_chain = create_retrieval_chain(
+        vectorstore.as_retriever(), combine_docs_chain
+    )
+    response = retrieval_chain.invoke({"input": question})
+    print(response["answer"])
 
 
 if __name__ == "__main__":
